a50c27162a5216fb776665351b285cfd7188dd28
rivanjuhan <rivannila26@gmail.com>
2024-09-29 19:47:15 +0700

    push github pertama

---
+++ script.js
@@ -0,0 +1,29 @@
+function cobainExternal(){
+// data awall
+  var name = "Nurul"  
+//   perbarui data
+    name = 'azizah'
+    // alert(name)
+
+// lock data dengan const
+// bertipe Immutable
+// data tidak bisa dirubah menggunakan const
+{const stringName = " iclik bozz" //bisa pakai petik 1 (') ataupun 2("")
+const angka = 1234;
+const gabung = stringName + angka
+const boolean = true // dapat bernilai true atau false
+const nulled = null
+
+// console.log('panggil didalam block =>',angka)
+}
+// console.log('panggil diluar block =>',angka)
+{let stringLet = 'Bekasi'
+stringLet = 'Bandung'
+
+console.log('panggil didalam block =>',stringLet) //jika menggunakan tanda kurung kurawal
+}
+// console.log('panggil diluar block =>',angka)
+
+// alert(boolean)
+
+}
